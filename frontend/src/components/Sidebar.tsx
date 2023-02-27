c1ed1cbf6cf3098ba76ff3c848e525ac2e1ef635
Timethief148 <92345597+Timethief148@users.noreply.github.com>
2023-02-27 16:17:16 -0600

    Sidebar is visible
    The sidebar is now visible with some text in it, currently has no application

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { SidebarData } from './SidebarData'
+
+
+
+const Sidebar: React.FunctionComponent = () => {
+    const [close, setClose] = useState(true)
+    const showSidebar = () => setClose(!close)
+    return (
+        <>
+        <SidebarMenu close={close}>
+            {SidebarData.map((item, index) => {
+                return (
+                    <MenuItems key={index}>
+                        <span style={{marginLeft: '16px'}}>{item.title}</span>
+
+                    </MenuItems>
+                )
+            })}
+        </SidebarMenu>
+        </>
+    )
+}
+export default Sidebar
 
 const Navbar = styled.div`
     display: flex;
@@ -9,13 +32,22 @@ const Navbar = styled.div`
     height: 3.5rem;
     background-color: #000080;
 `
+const MenuItems = styled.li`
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    width: 100%;
+    height: 90px;
+    padding: 1rem 0 1.25rem;
+`
 
-const Sidebar: React.FunctionComponent = () => {
-    return (
-        <>
-            <Navbar>
-
-            </Navbar>      
-        </>
-    )
-}
+const SidebarMenu = styled.div<{close: boolean}>`
+    width: 100px;
+    height: 100vh;
+    background-color: #000080;
+    position: fixed;
+    top: 0;
+    left: ${({ close}) => close ? '0' : '-100%'};
+    transition: .6s;
+`
