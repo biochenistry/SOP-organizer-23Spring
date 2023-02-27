417a22a9c2c888666d451eee20bc852d801957ef
Timethief148 <92345597+Timethief148@users.noreply.github.com>
2023-02-27 15:42:50 -0600

    Began work on Sidebar

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Navbar = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 3.5rem;
+    background-color: #000080;
+`
+
+const Sidebar: React.FunctionComponent = () => {
+    return (
+        <>
+            <Navbar>
+
+            </Navbar>      
+        </>
+    )
+}
