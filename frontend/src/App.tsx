d01f26518da098e0b96c0dd3d7de61495217e004
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2023-03-06 16:55:45 -0600

    Add basic login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import Header from './components/Header/Header'
 import { gql, useQuery } from "@apollo/client";
 import Sidebar from './components/Sidebar/Sidebar'
 import View from './components/View/View';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 const DEMO_QUERY = gql`
 query DemoQuery {
@@ -38,9 +40,13 @@ function App() {
         <Header />
         <View container flexDirection='row' height='100%'>
           <Sidebar />
-          <View padding='32px'>
+
+          <Routes>
+            <Route path='/login' element={<Login />} />
+          </Routes>
+          {/* <View padding='32px'>
             <iframe title='sop-document-embed' src='https://docs.google.com/document/d/1lG_U11017W_mKUPQLnA_rg_im3rXzSXiTSbM8i9U2s0/preview' style={{ width: '1000px', height: '100vh', border: 'none' }} />
-          </View>
+          </View> */}
         </View>
       </div >
     </>
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,69 @@
+import { gql, useMutation } from "@apollo/client";
+import Form from "../components/Form/Form";
+import useForm from "../components/Form/useForm";
+import TextField from "../components/TextField/TextField";
+import View from "../components/View/View";
+import { useState } from "react";
+import Paragraph from "../components/Paragraph/Paragraph";
+
+const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+  success: login(email: $email, password: $password)
+}
+`;
+
+type LoginResponse = {
+  success: boolean;
+}
+
+type LoginInput = {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const [login] = useMutation<LoginResponse>(LOGIN);
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const handleLogin = async (values: LoginInput) => {
+    const { data } = await login({
+      variables: {
+        email: values.email,
+        password: values.password,
+      },
+    });
+
+    console.log("HERE - OUTSIDe");
+
+    if (data?.success) {
+      window.location.reload();
+    } else {
+      console.log("HERE");
+      setHasError(true);
+    }
+  }
+
+  const loginForm = useForm<LoginInput>({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: handleLogin,
+  });
+
+  return (
+    <View container alignItems='center' justifyContent='center' width='100%'>
+      <View container>
+        <Form handleSubmit={loginForm.handleSubmit}>
+          <View container gap='16px' flexDirection='column'>
+            <TextField label='Email' name='email' type='text' value={loginForm.values.email} error={loginForm.errors.firstName} onChange={loginForm.handleChange} onValidate={loginForm.handleValidate} />
+            <TextField label='Password' name='password' type='password' value={loginForm.values.password} error={loginForm.errors.firstName} onChange={loginForm.handleChange} onValidate={loginForm.handleValidate} />
+            {hasError && <Paragraph>Invalid email or password</Paragraph>}
+            {/* TODO: Update to button component */}
+            <button type='submit'>Login</button>
+          </View>
+        </Form>
+      </View>
+    </View>
+  );
+}
