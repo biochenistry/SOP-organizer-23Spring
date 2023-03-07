a48830675f0c62e6f4564a0496850672d092ab7b
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2023-03-07 13:06:22 -0600

    #30 #29 Adds a logout and account settings button to header

--- frontend/src/components/Paragraph/Paragraph.tsx
+++ frontend/src/components/Paragraph/Paragraph.tsx
@@ -1,21 +1,26 @@
+import { CSSProperties, StyleSheet, css } from "aphrodite";
+import { Colors } from "../GlobalStyles";
+import { createStyle } from "../../util/createStyle";
+
 interface ParagraphProps {
-    children: React.ReactNode;
-    color?: string;
-    size?: number;
-    align?: 'left' | 'center' | 'right';
+  children: React.ReactNode;
+  style?: CSSProperties;
 }
 
-function Paragraph({ ...props }: ParagraphProps) {
-    return (
-        <p
-          style={{
-            fontSize: props.size || 16,
-            textAlign: props.align || 'left',
-            color: props.color || 'black',
-        }}>
-            {props.children}
-        </p>
-    )
+const styles = StyleSheet.create({
+  default: {
+    color: Colors.textPrimary,
+    fontSize: '16px',
+    textAlign: 'left',
+  },
+});
+
+function Paragraph(props: ParagraphProps) {
+  return (
+    <p className={css(styles.default, createStyle(props.style))}>
+      {props.children}
+    </p>
+  )
 }
 
 export default Paragraph;
