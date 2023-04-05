7311e7957a70f9b4094938324abc535edf104578
Timethief148 <92345597+Timethief148@users.noreply.github.com>
2023-04-05 14:46:07 -0500

    minor edits

--- frontend/src/components/SearchBar/Searchbar.tsx
+++ frontend/src/components/SearchBar/Searchbar.tsx
@@ -3,12 +3,12 @@ import {IoIosSearch} from 'react-icons/io'
 import View from '../View/View'
 
 const Searchbar: React.FunctionComponent = () => {
-    
+    const search = () => ([]);
     return (
         <View padding='0 0 4px 0'>
             <div>
                 <input type = "text" className = "input" placeholder="Search..." />
-                    <button>Search</button>            
+                    <button onClick={search}>Search</button>            
             </div>
         </View>
     )
