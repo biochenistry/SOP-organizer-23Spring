afe2774fafd15054186d35498137b81208b53ebb
Evan Lewis <36089251+lewisevan@users.noreply.github.com>
2023-04-12 17:57:25 -0500

    Code format changes

--- frontend/src/pages/AddUser.tsx
+++ frontend/src/pages/AddUser.tsx
@@ -1,16 +1,9 @@
-import { useNavigate } from "react-router";
 import View from "../components/View/View";
 import { gql, useMutation } from "@apollo/client";
-import { useAuthState } from "../components/Auth";
 import Button from "../components/Button/Button";
 import Form from "../components/Form/Form";
 import useForm from "../components/Form/useForm";
 import TextField from "../components/TextField/TextField";
-import { useEffect, useState } from "react";
-import { StyleSheet, css } from 'aphrodite';
-import { Colors } from '../components/GlobalStyles';
-import { createStyle } from '../util/createStyle';
-import Paragraph from "../components/Paragraph/Paragraph";
 
 
 const ADD_USER = gql`
@@ -25,81 +18,78 @@ mutation createUser($firstname: String!, $lastname: String!, $username: String!,
 `;
 
 type CreateUserResponse = {
-    user: User;
+  user: User;
 }
 
 type CreateUserInput = {
-    firstname: string;
-    lastname: string;
-    username: string;
-    password: string;
-    admin: boolean;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  admin: boolean;
 }
 
 type User = {
-    id: string,
-    firstName: string;
-    lastName: string;
-    username: string;
-    isDisabled: boolean;
-    isAdmin: boolean;
+  id: string,
+  firstName: string;
+  lastName: string;
+  username: string;
+  isDisabled: boolean;
+  isAdmin: boolean;
 }
 
 
 export default function AddUser() {
-    const navigate = useNavigate();
-    const { state } = useAuthState();
-    const [createUser, { data }] = useMutation<CreateUserResponse>(ADD_USER, { errorPolicy: 'all' });
+  const [createUser, { data }] = useMutation<CreateUserResponse>(ADD_USER, { errorPolicy: 'all' });
 
-    const handleCreateUser = async (values: CreateUserInput) => {
-        const { data } = await createUser({
-          variables: {
-            firstname: values.firstname,
-            lastname: values.lastname,
-            username: values.username,
-            password: values.password,
-            admin: values.admin,
-          },
-        });
-      }
+  const handleCreateUser = async (values: CreateUserInput) => {
+    await createUser({
+      variables: {
+        firstname: values.firstname,
+        lastname: values.lastname,
+        username: values.username,
+        password: values.password,
+        admin: values.admin,
+      },
+    });
+  }
 
-      const createUserForm = useForm<CreateUserInput>({
-        initialValues: {
-          firstname: '',
-          lastname: '',
-          username: '',
-          password: '',
-          admin: false,
-        },
-        onSubmit: handleCreateUser,
-      });
+  const createUserForm = useForm<CreateUserInput>({
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      username: '',
+      password: '',
+      admin: false,
+    },
+    onSubmit: handleCreateUser,
+  });
 
-    //plug in the new info-runs when a new user is returned, so remind them to add the user to google drive
-    //issue that user does not reload
-      if (data?.user) {
-        return (
-            <p>Don't forget to add the user to the google folder!</p>
-        );
-      }
-
-      
-    //Returns a form to fill out users information
-    return (      
-        <View container alignItems='center' justifyContent='center' width='100%' flexDirection="column">
-        <Button label='Cancel' href='/users' variant='secondary' onDark type='submit' style={{ width: '20%' }} />
-
-        <View>
-            <Form handleSubmit={createUserForm.handleSubmit}>
-                <View container gap= '8px' flexDirection="column">
-                    <TextField label='First Name' name='firstname' type='text' value={createUserForm.values.firstname} error={createUserForm.errors.firstname} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
-                    <TextField label='Last Name' name='lastname' type='text' value={createUserForm.values.lastname} error={createUserForm.errors.lastname} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
-                    <TextField label='Username' name='username' type='text' value={createUserForm.values.username} error={createUserForm.errors.username} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
-                    <TextField label='Password' name='password' type='text' value={createUserForm.values.password} error={createUserForm.errors.password} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
-                    <Button label='CreateUser' variant='primary' type='submit' />
-                </View>
-            </Form>
-        </View>
-        </View> 
+  //plug in the new info-runs when a new user is returned, so remind them to add the user to google drive
+  //issue that user does not reload
+  if (data?.user) {
+    return (
+      <p>Don't forget to add the user to the google folder!</p>
     );
-}  
-  
+  }
+
+
+  //Returns a form to fill out users information
+  return (
+    <View container alignItems='center' justifyContent='center' width='100%' flexDirection="column">
+      <Button label='Cancel' href='/users' variant='secondary' onDark type='submit' style={{ width: '20%' }} />
+
+      <View>
+        <Form handleSubmit={createUserForm.handleSubmit}>
+          <View container gap='8px' flexDirection="column">
+            <TextField label='First Name' name='firstname' type='text' value={createUserForm.values.firstname} error={createUserForm.errors.firstname} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
+            <TextField label='Last Name' name='lastname' type='text' value={createUserForm.values.lastname} error={createUserForm.errors.lastname} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
+            <TextField label='Username' name='username' type='text' value={createUserForm.values.username} error={createUserForm.errors.username} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
+            <TextField label='Password' name='password' type='text' value={createUserForm.values.password} error={createUserForm.errors.password} onChange={createUserForm.handleChange} onValidate={createUserForm.handleValidate} required />
+            <Button label='CreateUser' variant='primary' type='submit' />
+          </View>
+        </Form>
+      </View>
+    </View>
+  );
+}
--- frontend/src/pages/UsersList.tsx
+++ frontend/src/pages/UsersList.tsx
@@ -3,31 +3,30 @@ import View from "../components/View/View";
 import { gql, useQuery, useMutation } from '@apollo/client'
 import { useAuthState } from "../components/Auth";
 import Button from "../components/Button/Button";
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet } from 'aphrodite';
 import { Colors } from '../components/GlobalStyles';
-import { createStyle } from '../util/createStyle';
 
 
 const styles = StyleSheet.create({
-    usertable: {
-        padding: '16px',
-        'user-select': 'none',
-        ':hover': {
-          backgroundColor: Colors.neutralHover,
-          cursor: 'pointer',
-        },
-        ':active': {
-          backgroundColor: Colors.neutralActive,
-        
-        },
+  usertable: {
+    padding: '16px',
+    'user-select': 'none',
+    ':hover': {
+      backgroundColor: Colors.neutralHover,
+      cursor: 'pointer',
     },
-    table: {
-        backgroundColor: Colors.isuRed,
-        borderBottom: `4px solid ${Colors.isuYellow}`,
-        color: '#ffffff',
-        width: '100%',
-        padding: '15px',
-    }
+    ':active': {
+      backgroundColor: Colors.neutralActive,
+
+    },
+  },
+  table: {
+    backgroundColor: Colors.isuRed,
+    borderBottom: `4px solid ${Colors.isuYellow}`,
+    color: '#ffffff',
+    width: '100%',
+    padding: '15px',
+  }
 });
 
 
@@ -64,127 +63,127 @@ mutation deleteUser($userId: ID!){
 `
 
 type RemoveUserResponse = {
-    success: boolean;
+  success: boolean;
 }
 
 type RemoveUserInput = {
-    userId: string;
+  userId: string;
 }
 
 type MakeAdminResponse = {
-    user: User;
+  user: User;
 }
 
 type MakeAdminInput = {
-    userId: string;
-    admin: boolean;
+  userId: string;
+  admin: boolean;
 }
 
 
 type GetAllUsersResponse = {
-    all: User[] | null;
+  all: User[] | null;
 }
 
 
 type User = {
-    id: string,
-    firstName: string;
-    lastName: string;
-    username: string;
-    isDisabled: boolean;
-    isAdmin: boolean;
+  id: string,
+  firstName: string;
+  lastName: string;
+  username: string;
+  isDisabled: boolean;
+  isAdmin: boolean;
 }
 
-function determineAdmin(person: User){
-    if (person.isAdmin){
-        return "Admin";
-    }
-    else{
-        return "Not Admin";
-    }
+function determineAdmin(person: User) {
+  if (person.isAdmin) {
+    return "Admin";
+  }
+  else {
+    return "Not Admin";
+  }
 }
 
-function adminButtonLabel(person: User){
-    if(person.isAdmin){
-        return "Make User";
-    }
-    else{
-        return "Make Admin";
-    }
+function adminButtonLabel(person: User) {
+  if (person.isAdmin) {
+    return "Make User";
+  }
+  else {
+    return "Make Admin";
+  }
 }
 
 
 
 //allows admin users to access user information
 const Page: React.FunctionComponent = () => {
-    const navigate = useNavigate();
-    const { state } = useAuthState();
-    const { data, refetch: refetchUsers } = useQuery<GetAllUsersResponse>(GET_ALL_USERS, {
-        fetchPolicy: "network-only"
+  const navigate = useNavigate();
+  const { state } = useAuthState();
+  const { data, refetch: refetchUsers } = useQuery<GetAllUsersResponse>(GET_ALL_USERS, {
+    fetchPolicy: "network-only"
+  });
+  const [makeAdmin, { loading: isMakeAdminLoading }] = useMutation<MakeAdminResponse>(MAKE_ADMIN);
+  const [removeUser, { loading: isRemoveUserLoading }] = useMutation<RemoveUserResponse>(REMOVE_USER);
+
+
+
+  const handleChangeRole = async (values: MakeAdminInput) => {
+    await makeAdmin({
+      variables: {
+        userId: values.userId,
+        admin: values.admin,
+      },
     });
-    const [makeAdmin, {loading: isMakeAdminLoading}] = useMutation<MakeAdminResponse>(MAKE_ADMIN);
-    const [removeUser, {loading: isRemoveUserLoading}] = useMutation<RemoveUserResponse>(REMOVE_USER);
+  }
+
+  const handleRemoveUser = async (values: RemoveUserInput) => {
+    await removeUser({
+      variables: {
+        userId: values.userId,
+      },
+    });
+
+    await refetchUsers();
+  }
 
 
-    
-    const handleChangeRole = async (values: MakeAdminInput) => {
-        const { data } = await makeAdmin({
-          variables: {
-            userId: values.userId,
-            admin: values.admin,
-          },
-        });  
-    }
+  if (!state.user?.isAdmin) {
+    navigate('/');
+  }
 
-    const handleRemoveUser = async (values: RemoveUserInput) => {
-        const { data } = await removeUser({
-          variables: {
-            userId: values.userId,
-          },
-        });
+  //Returns a table with each user's information
+  return (
+    <View container alignItems='center' justifyContent='center' width='100%' flexDirection="column">
+      <Button label='Add User' href='/adduser' variant='secondary' onDark type='submit' style={{ width: '20%' }} />
 
-        await refetchUsers();
-    }
-    
-
-    if (!state.user?.isAdmin) {
-        navigate('/');
-    }
-
-    //Returns a table with each user's information
-    return (      
-        <View container alignItems='center' justifyContent='center' width='100%' flexDirection="column">
-        <Button label='Add User' href='/adduser' variant='secondary' onDark type='submit' style={{ width: '20%' }} />
-
-        <table style={{ borderBottom: `4px solid ${Colors.isuYellow}`, width: '80%', padding: '15px', }} >
-            <thead>
-            <tr>
-                <th style={{textAlign: 'left'}}>First Name</th>
-                <th style={{textAlign: 'left'}}>Last Name</th>
-                <th style={{textAlign: 'left'}}>Username</th>
-                <th style={{textAlign: 'left'}}>Admin?</th>
-            </tr>
-            </thead>
-            <tbody>
-            {data?.all?.map((user, index) => {
-                    return(
-                    <tr key={index}>
-                        <td>{user.firstName}</td> 
-                        <td>{user.lastName}</td> 
-                        <td>{user.username}</td> 
-                        <td>{determineAdmin(user)} </td> 
-                        <td>
-                            <Button label='Remove' variant='secondary' onDark type='submit' style={{ width: '100%' }} onClick={()=>{handleRemoveUser({userId: user.id})}} isLoading = {isRemoveUserLoading}/>
-                        </td>
-                        <td>
-                            <Button label={adminButtonLabel(user)} variant='secondary' onDark style={{ width: '100%' }} onClick={()=>{handleChangeRole({userId: user.id, admin: !user.isAdmin })}} isLoading= {isMakeAdminLoading}/>
-                        </td>
-                    </tr>
-                    );
-                })}
+      <table style={{ borderBottom: `4px solid ${Colors.isuYellow}`, width: '80%', padding: '15px', }} >
+        <thead>
+          <tr>
+            <th style={{ textAlign: 'left' }}>First Name</th>
+            <th style={{ textAlign: 'left' }}>Last Name</th>
+            <th style={{ textAlign: 'left' }}>Username</th>
+            <th style={{ textAlign: 'left' }}>Admin?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.all?.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.username}</td>
+                <td>{determineAdmin(user)} </td>
+                <td>
+                  <Button label='Remove' variant='secondary' onDark type='submit' style={{ width: '100%' }} onClick={() => { handleRemoveUser({ userId: user.id }) }} isLoading={isRemoveUserLoading} />
+                </td>
+                <td>
+                  <Button label={adminButtonLabel(user)} variant='secondary' onDark style={{ width: '100%' }} onClick={() => { handleChangeRole({ userId: user.id, admin: !user.isAdmin }) }} isLoading={isMakeAdminLoading} />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
-        </table>
-        </View> 
-    );
-}  
+      </table>
+    </View>
+  );
+}
 export default Page
